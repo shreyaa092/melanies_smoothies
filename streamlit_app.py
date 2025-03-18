2b01aa18b840d59cf01f619fdee8d5312ac73837
Shreya Girotra <147151680+shreyaa092@users.noreply.github.com>
2025-03-18 14:42:29 +0530

    Update streamlit_app.py
    SmoothieFroot Data to Show Nutrition Data for the Fruits Chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,13 @@ if ingredients_list:
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+if ingredients_list:
+    ingredients_string= ' '
+
+    for fruit_chose in ingredients_list:
+        ingredients_string += fruit_chosen + ' ' 
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
