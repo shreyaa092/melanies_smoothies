b14bbe38bd5d9b239b483c8f516d2838a6c14631
Shreya Girotra <147151680+shreyaa092@users.noreply.github.com>
2025-03-21 15:25:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ if ingredients_list:
     st.write(ingredients_string)  # Print concatenated ingredients
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('""" + ingredients_string + name_on_order+"""')"""
+            values ('""" + ingredients_string+"""','""" + name_on_order+"""' )"""
 
     st.write(my_insert_stmt)
     #st.stop()
